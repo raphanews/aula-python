9889f244d98d37965ff9992003e0c6af5ab98aa3
rafael <rafael@prodados.inf.br>
2021-09-22 14:55:43 -0300

    atualização

--- testes.py
+++ testes.py
@@ -1,13 +1,13 @@
 #testando classe
 import unittest
 
-class MyFun:
+class MyFun1:
     def fun(self, n):
         return n + 1
 
 class MyFunTest(unittest.TestCase):
     def testFun(self):
-        obj = MyFun()
+        obj = MyFun1()
         self.assertEqual(obj.fun(3), 4)
 
 if __name__ == '__main__':
